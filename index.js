c8be61be7bad2813f682133616062a5ebb723257
alexander-morris <alex@theblockchaininstitute.org>
2020-01-02 15:22:44 -0600

    Adding handling for .env

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
-var port = 3000
+const dotenv = require('dotenv')
+dotenv.config({ path: '.env' })
+
+const port = process.env.PORT || 3000;
+
+console.log('process is ', process.env)
 
 app.get('/', (req, res) => res.send('hello al!'))
 
