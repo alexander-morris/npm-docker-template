8d455f83e160edf239664e242bb0673b0c0d7a06
alexander-morris <alex@theblockchaininstitute.org>
2020-01-02 15:12:49 -0600

    Fixed eslint run

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const app = express();
-var port = 3000;
+const express = require('express')
+const app = express()
+var port = 3000
 
-app.get('/', (req, res) => res.send('hello al!'));
+app.get('/', (req, res) => res.send('hello al!'))
 
 app.listen(port, () => {
-    console.log('MY REST API IS RUNNING ON PORT ' + port)
+  console.log('MY REST API IS RUNNING ON PORT ' + port)
 })
